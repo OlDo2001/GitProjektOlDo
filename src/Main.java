433af5a9e855cc6194907cd8bab23bd8623b10fa
Olga Domorod <olga.domorod@gmail.com>
2020-10-21 11:48:08 +0200

    skriva mer

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("Hej Olga!");
+        System.out.println("Skriva lite mer");
     }
 }
